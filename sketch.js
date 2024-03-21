e46294a2b9a9e0945b77985f40dce23a4e23cd4d
ctlyncui <jc9304@nyu.edu>
2024-03-21 11:07:23 -0400

    Exploring the Secretary, independent study @Spring2023

--- sketch.js
+++ sketch.js
@@ -1,214 +1,297 @@
-// variable to hold a reference to our A-Frame world
+// variables
 let world;
+let container;
+let cabinet;
 
-// variables to hold our models
+//background music
+let bgm;
+let playBgm = 0;
+
+// secretary models
 let secretary, drawer;
 let drawerContainer;
 let clickRegion1;
 
-// animation for the drawer
-let animationMode = false;
-
-// drawers
+//drawers
 let drawerTop;
 let drawerLeft;
 let drawerRight;
 
+var animationMode = false;
+
+function preload(){
+	bgm = loadSound('audio/intro.mp3');
+}
+
 function setup() {
-	// no canvas needed
-	noCanvas();
+    	let cnv =createCanvas(512,512).id();
 
-	// construct the A-Frame world
-	// this function requires a reference to the ID of the 'a-scene' tag in our HTML document
-	world = new World('VRScene');
+    	// construct the A-Frame world
+    	// this function requires a reference to the ID of the 'a-scene' tag in our HTML document
+    	world = new World('VRScene');
+        // turn off WASD
+    	world.camera.cameraEl.removeAttribute('wasd-controls')
 
-	// set up a background color
-	world.setBackground(128,128,128);
+    	let floor = new Plane({
+    		x:0,y:0,z:-20,
+    		asset:'bgr2',
+    		width:800, height:600,
+            scaleX: 0.08,
+            scaleY: 0.08
+    	});
+    	world.add(floor);
 
-	// create a base plane
-	var basePlane = new Plane({
-		x: 0, y:0, z:0, 
-		width:100, height:100, 
-		rotationX:-90,
-		red: 10, green: 10, blue: 10
-	});
-	world.add(basePlane);
+        container = new Container3D({
+            x: -3.5,
+            y: -8.4,
+            z:-6.5,
+            scaleX:3,
+            scaleY:3,
+            scaleZ:3
+        });
+        world.add(container)
 
-	// add a Wavefront (OBJ) model
-	// you need to make sure to reference both the OBJ and MTL file here (geometry & material are stored separately)
-	// refer to the 'index.html' file to see how these were pre-loaded into the scene
-	secretary = new OBJ({
-		asset: 'robot_obj',
-		mtl: 'robot_mtl',
-		x: 0,
-		y: 0,
-		z: 0,
-		rotationX:0,
-		scaleX:0.03,
-		scaleY:0.03,
-		scaleZ:0.03,
-	});
-	world.add(secretary);
+        // add a Wavefront (OBJ) model
+        // you need to make sure to reference both the OBJ and MTL file here (geometry & material are stored separately)
+        secretary = new OBJ({
+            asset: 'robot_obj',
+            mtl: 'robot_mtl',
+            x: 0,
+            y: 0,
+            z: 0,
+            rotationX:0,
+            scaleX:0.03,
+            scaleY:0.03,
+            scaleZ:0.03,
+        });
+        container.add(secretary);
 
-	clickRegion1 = new Box({
-		x: 1.05, y: 1.5, z: -0.3,
-		red: 255, green: 255, blue: 0,
-		width: 0.5, height: 0.5, depth: 0.5,
-		rotationX: -35,
-		opacity: 0.2,
-		clickFunction: function(e) {
-			animationMode = true;
-			world.remove(e);
-		}
-	});
-	world.add(clickRegion1);
+	    const rotateLeftBtn = new Tetrahedron({
+            x: -4,
+            y: 0,
+            z: 0,
+            radius: 0.5,
+            red: 207,
+            green: 185,
+            blue: 151,
+            enterFunction: function (me) {
+                me.setScale(1.5, 1.5, 1.5);
+            },
+            leaveFunction: function (me) {
+                me.setScale(1, 1, 1);
+            },
+            clickFunction: (me) => {
+                if(container.rotationY < 50){
+                	container.spinY(8);
+                }
+            },
+        });
 
-	drawer = new OBJ({
-		asset: 'drawer_obj',
-		mtl: 'drawer_mtl',
-		x: 0,
-		y: 0,
-		z: 0,
-		scaleX:0.03,
-		scaleY:0.03,
-		scaleZ:0.03,
-	});
-	drawerContainer = new Container3D({
-		x: 0.9,
-		y: 1.1,
-		z: -0.3,
-		rotationX:-35
-	})
-	world.add(drawerContainer);
-	drawerContainer.add(drawer);
+        const rotateRightBtn = new Tetrahedron({
+            x: 4,
+            y: 0,
+            z: 0,
+            radius: 0.5,
+            red: 207,
+            green: 185,
+            blue: 151,
+            enterFunction: function (me) {
+                me.setScale(1.5, 1.5, 1.5);
+            },
+            leaveFunction: function (me) {
+                me.setScale(1, 1, 1);
+            },
+            clickFunction: (me) => {
+                if(container.rotationY > -8){
+                	container.spinY(-5);
+                }
+            },
+        });
 
-	drawerTop = new Box({
-		red: 0, blue: 255, green: 0,
-		x: -0.2, y: 0.45, z: 1.4,
-		width: 0.3, height: 0.05, depth: 0.5,
-		opacity: 0,
-		clickFunction: function(e) {
-			e.clicked = true;
-		}
-	});
-	drawerTop.movementDirection = 0.01;
-	drawerTop.clicked = false;
-	drawerContainer.add(drawerTop);
+        world.add(rotateLeftBtn)
+        world.add(rotateRightBtn)
 
-	drawerLeft = new Box({
-		red: 0, blue: 0, green: 255,
-		x: -0.35, y: 0.3, z: 1.4,
-		rotationZ: 90,
-		width: 0.4, height: 0.05, depth: 0.5,
-		opacity: 0,
-		clickFunction: function(e) {
-			e.clicked = true;
-		}
-	});
-	drawerLeft.movementDirection = 0.01;
-	drawerLeft.clicked = false;
-	drawerContainer.add(drawerLeft);
+        clickRegion1 = new Box({
+            // x: 1.05, y: 1.5, z: -0.3,
+            x: 1.05, y:1.5, z: -0.3,
+            red: 255, green: 255, blue: 0,
+            width: 0.5, height: 0.5, depth: 0.5,
+            rotationX: -35,
+            opacity: 0.2,
+            clickFunction: function(e) {
+                animationMode = true;
+                world.remove(e);
+            }
+        });
+        container.add(clickRegion1);
 
-	drawerRight = new Box({
-		red: 255, blue: 0, green: 0,
-		x: -0.1, y: 0.3, z: 1.4,
-		rotationZ: 90,
-		width: 0.4, height: 0.05, depth: 0.5,
-		opacity: 0,
-		clickFunction: function(e) {
-			e.clicked = true;
-		}
-	});
-	drawerRight.movementDirection = 0.01;
-	drawerRight.clicked = false;
-	drawerContainer.add(drawerRight);
+        drawer = new OBJ({
+            asset: 'drawer_obj',
+            mtl: 'drawer_mtl',
+            x: 0,
+            y: 0,
+            z: 0,
+            scaleX:0.03,
+            scaleY:0.03,
+            scaleZ:0.03,
+        });
+
+        drawerContainer = new Container3D({
+            //0.9, 1.1, -0.3
+            x: 0.9,
+            y: 1.1,
+            z: -0.3,
+            rotationX:-35
+        })
+        container.add(drawerContainer);
+        drawerContainer.add(drawer);
+
+        drawerTop = new Box({
+            red: 0, blue: 255, green: 0,
+            //-0.2 0.45 1.4
+            x: -0.2, y: 0.45, z: 1.4,
+            width: 0.3, height: 0.05, depth: 0.5,
+            opacity: 0,
+            clickFunction: function(e) {
+                e.clicked = true;
+            }
+        });
+        drawerTop.movementDirection = 0.01;
+        drawerTop.clicked = false;
+        drawerContainer.add(drawerTop);
+
+        drawerLeft = new Box({
+            red: 0, blue: 0, green: 255,
+            //-0.35 0.3 1.4
+            x: -0.35, y: 0.3, z: 1.4,
+            rotationZ: 90,
+            width: 0.4, height: 0.05, depth: 0.5,
+            opacity: 0,
+            clickFunction: function(e) {
+                e.clicked = true;
+            }
+        });
+
+        drawerLeft.movementDirection = 0.01;
+        drawerLeft.clicked = false;
+        drawerContainer.add(drawerLeft);
+
+        drawerRight = new Box({
+            red: 255, blue: 0, green: 0,
+            //-0.1 0.3 1.4
+            x: -0.1, y: 0.3, z: 1.4,
+            rotationZ: 90,
+            width: 0.4, height: 0.05, depth: 0.5,
+            opacity: 0,
+            clickFunction: function(e) {
+                e.clicked = true;
+            }
+        });
+        drawerRight.movementDirection = 0.01;
+        drawerRight.clicked = false;
+        drawerContainer.add(drawerRight);
 
 
-	// an ambient light - this light has no position and lights all entities equally
-	light2 = new Light({
-		color: '#fff',
-		type: 'ambient',
-		intensity: 8
-	})
-	world.add(light2)
+        // an ambient light - this light has no position and lights all entities equally
+        light2 = new Light({
+            color: '#fff',
+            type: 'ambient',
+            intensity: 0.9,
+            x:0,
+            y:0,
+            z:0
+        })
+        container.add(light2)
 
+}
+
+function switchBgm(e) {
+    e.target.children.forEach((c) => c.classList.toggle('hidden'));
+    if (!bgm || !bgm.isLoaded()) {
+        console.log('bgm not ready');
+    } else {
+        playBgm ? bgm.pause() : bgm.play();
+        playBgm ^= 1;
+    }
 }
 
 function draw() {
-	if (animationMode) {
-		popOutDrawer();
-	}
-	if (drawerTop.clicked) {
-		drawerTop.nudge(0,0,drawerTop.movementDirection);
-		if (drawerTop.movementDirection > 0 && drawerTop.getZ() > 1.9) {
-			drawerTop.clicked = false;
-			drawerTop.movementDirection = -0.01;
-		}
-		else if (drawerTop.movementDirection < 0 && drawerTop.getZ() <= 1.4) {
-			drawerTop.clicked = false;
-			drawerTop.movementDirection = 0.01;
-		}
-	}
-	if (drawerLeft.clicked) {
-		drawerLeft.nudge(0,0,drawerLeft.movementDirection);
-		if (drawerLeft.movementDirection > 0 && drawerLeft.getZ() > 1.9) {
-			drawerLeft.clicked = false;
-			drawerLeft.movementDirection = -0.01;
-		}
-		else if (drawerLeft.movementDirection < 0 && drawerLeft.getZ() <= 1.4) {
-			drawerLeft.clicked = false;
-			drawerLeft.movementDirection = 0.01;
-		}
-	}
+    if (animationMode) {
+        popOutDrawer();
+        // popInDrawer();
+    }
 
-	if (drawerRight.clicked) {
-		drawerRight.nudge(0,0,drawerRight.movementDirection);
-		if (drawerRight.movementDirection > 0 && drawerRight.getZ() > 1.9) {
-			drawerRight.clicked = false;
-			drawerRight.movementDirection = -0.01;
-		}
-		else if (drawerRight.movementDirection < 0 && drawerRight.getZ() <= 1.4) {
-			drawerRight.clicked = false;
-			drawerRight.movementDirection = 0.01;
-		}
-	}
+    //move the small counterpart
+    if (drawerTop.clicked) {
+        drawerTop.nudge(0,0,drawerTop.movementDirection);
+        if (drawerTop.movementDirection > 0 && drawerTop.getZ() > 1.9) {
+            drawerTop.clicked = false;
+            drawerTop.movementDirection = -0.01;
+        }
+        else if (drawerTop.movementDirection < 0 && drawerTop.getZ() <= 1.4) {
+            drawerTop.clicked = false;
+            drawerTop.movementDirection = 0.01;
+        }
+    }
+    if (drawerLeft.clicked) {
+        drawerLeft.nudge(0,0,drawerLeft.movementDirection);
+        if (drawerLeft.movementDirection > 0 && drawerLeft.getZ() > 1.9) {
+            drawerLeft.clicked = false;
+            drawerLeft.movementDirection = -0.01;
+        }
+        else if (drawerLeft.movementDirection < 0 && drawerLeft.getZ() <= 1.4) {
+            drawerLeft.clicked = false;
+            drawerLeft.movementDirection = 0.01;
+        }
+    }
 
+    if (drawerRight.clicked) {
+        drawerRight.nudge(0,0,drawerRight.movementDirection);
+        if (drawerRight.movementDirection > 0 && drawerRight.getZ() > 1.9) {
+            drawerRight.clicked = false;
+            drawerRight.movementDirection = -0.01;
+        }
+        else if (drawerRight.movementDirection < 0 && drawerRight.getZ() <= 1.4) {
+            drawerRight.clicked = false;
+            drawerRight.movementDirection = 0.01;
+        }
+    }
 }
-
 
 let drawerPopOutCounter = 0;
 function popOutDrawer() {
-	if (drawerPopOutCounter < 100) {
-		drawer.nudge(0,0,0.01);
-		drawerPopOutCounter++;
-	}
-	else if (drawerPopOutCounter < 135) {
-		drawerContainer.spinX(1);
-		drawerPopOutCounter++;
-	}
-	else if (drawerPopOutCounter < 135+180) {
-		drawer.spinY(1);
-		drawerPopOutCounter++;
-	}
-	else {
-		drawerTop.setOpacity(1.0);
-		drawerLeft.setOpacity(1.0);
-		drawerRight.setOpacity(1.0);
-		animationMode = false;
-	}
+
+    if (drawerPopOutCounter < 100) {
+        drawer.nudge(0,0,0.01);
+        drawerPopOutCounter++;
+    }
+    else if (drawerPopOutCounter < 135) {
+        drawerContainer.spinX(1);
+        drawerPopOutCounter++;
+    }
+    else if (drawerPopOutCounter < 135+180) {
+        drawer.spinY(1);
+        drawerPopOutCounter++;
+    }
+    else {
+        drawerTop.setOpacity(1.0);
+        drawerLeft.setOpacity(1.0);
+        drawerRight.setOpacity(1.0);
+        animationMode = false;
+    }
 }
 function popInDrawer() {
-	if (drawerPopOutCounter >= 135) {
-		drawer.spinY(-1);
-		drawerPopOutCounter--;
-	}
-	else if (drawerPopOutCounter >= 100) {
-		drawerContainer.spinX(-1);
-		drawerPopOutCounter--;
-	}
+    if (drawerPopOutCounter >= 135) {
+        drawer.spinY(-1);
+        drawerPopOutCounter--;
+    }
+    else if (drawerPopOutCounter >= 100) {
+        drawerContainer.spinX(-1);
+        drawerPopOutCounter--;
+    }
 
-	else if (drawerPopOutCounter >= 0) {
-		drawer.nudge(0,0,-0.01);
-		drawerPopOutCounter--;
-	}
+    else if (drawerPopOutCounter >= 0) {
+        drawer.nudge(0,0,-0.01);
+        drawerPopOutCounter--;
+    }
 }
